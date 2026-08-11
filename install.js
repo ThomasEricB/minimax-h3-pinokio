9c81a56ad8647152e13c6b22a41035985609bd33
6Morpheus6 <wanton-decay@gmx.net>
2026-08-11 19:38:21 +0200

    install kjnodes only for nvidia

--- install.js
+++ install.js
@@ -34,6 +34,7 @@ module.exports = {
     },
     // Install KJNodes
     {
+      when: "{{platform === 'win32' && gpu === 'nvidia''}}",
       method: "shell.run",
       params: {
         path: "app/custom_nodes",
@@ -43,6 +44,7 @@ module.exports = {
       }
     },
     {
+      when: "{{platform === 'win32' && gpu === 'nvidia''}}",
       method: "shell.run",
       params: {
         venv: "../../env",
